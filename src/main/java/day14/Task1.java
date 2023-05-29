2753e18333c5866673ec5ef782efc38667735e55
Daniil <daniilsuvorov1999@mail.ru>
2023-05-29 14:43:42 +0300

    add day 14

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,40 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("test.txt");
+        printSumDigits(file);
+
+    }
+
+    public static void printSumDigits(File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            String[] array = line.split(" ");
+            if (array.length != 10) {
+                try {
+                    throw new IOException();
+                } catch (IOException e) {
+                    System.out.println("Некорректный входной файл");
+                }
+            } else {
+                int sum = 0;
+                for (String x : array) {
+                    sum += Integer.parseInt(x);
+                }
+                System.out.println(sum);
+            }
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
 
     }
 }
--- src/main/java/day14/Task2.java
+++ src/main/java/day14/Task2.java
@@ -1,7 +1,46 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        File file = new File("people.txt");
+        System.out.println(parseFileToStringList(file));
 
     }
+
+    public static List<String> parseFileToStringList(File file) {
+        List<String> al = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(file);
+            String string = "";
+            String[] array = new String[2];
+            while (scanner.hasNextLine()) {
+                array = scanner.nextLine().split(" ");
+                if (Integer.parseInt(array[1]) < 0) {
+                    try {
+                        throw new IOException();
+                    } catch (IOException e) {
+                        System.out.println("Некорректный ввод");
+                        al.clear();
+                        break;
+                    }
+                } else {
+                    string = array[0] + " " + array[1];
+                    al.add(string);
+                }
+
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+        return al;
+    }
 }
+
